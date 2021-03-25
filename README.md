2d8937561b9e37f531c94892274888cdbcbbffea
Aaron Fink <apf75@cornell.edu>
2021-03-25 16:04:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Conditionals Lab
+
+[![Run on Repl.it](https://repl.it/badge/github/upperlinecode/conditionals-lab-javascript)](https://repl.it/github/upperlinecode/conditionals-lab-javascript)
+
+## TL;DR
+
+Follow the instructions in conditionals.js to get more comfortable using if, else, and else-if statements. You'll have to uncomment the code associated with each challenge as you progress.
+
+## Contents
+
+1. [Intro](#intro)
+2. [The Lab](#the-lab)
+
+## Intro
+
+Conditionals are a super important tool in your toolbox, as they allow the program's you right to react dynamically to variable circumstances. The challenges outlined in conditionals.js will give you numerous examples of where conditionals are necessary.
+
+## The Lab
+
+Open up conditionals.js and go through the challenges one at a time. The code associated with each challenge is commented to avoid clogging up your output; so uncomment it first, and then when your code works as expected, comment it again and move on.
---
+++ conditionals.js
@@ -0,0 +1,91 @@
+// Replace the '__' with comparison operators to make the following statements true
+// Try to use a different comparison operator each time
+// console.log(45 __ 45)
+// console.log(-9 __ 9)
+// console.log(100 __ 100000)
+// console.log(5.6 __ 5.5)
+// console.log("hello" __ "dog")
+// console.log("Danny" __ "Danny")
+
+// Replace the '__' with comparison operators to make the following statements false
+// Try to use a different comparison operator each time
+// console.log(2 __ 10)
+// console.log(22 __ 22)
+// console.log( 0 __ -5)
+// console.log(3.3 __ 3.5)
+// console.log("frog" __ "prince")
+
+// -----------------------------------------------------------------------------------
+
+// replace the blank with a conditional statement so that the lunch invitation is only printed if isHungry is true. Change the boolean in the isHungry variable to check and see if your conditional statement works.
+let isHungry = false
+
+// if (__________) {
+//     console.log("Do you want to grab some lunch?")
+// }
+
+// replace the blank with a conditional statement that tests to see if a number is positive or negative.
+let firstNumber //  = ______
+// if (__________) {
+//     console.log("That number is negative!")
+// } else {
+//     console.log("That number is positive!")
+// }
+
+
+// replace the blank with a conditional statement that tests to see if a number is even. Hint: the % operator might be useful here.
+let secondNumber: //  = ______
+// if (__________) {
+//     console.log("That number is even!")
+// } else {
+//     console.log("That number is odd!")
+// }
+
+
+
+
+
+// replace the blank with a conditional statement that only adds a fee to the ticket price if the luggage weight is over 50 pounds
+let luggageWeight = 55
+const ticketPrice = 150
+
+// if (__________) {
+//     ticketPrice += 20
+//     console.log("An extra $20.00 fee was added to the plane ticket for going over the maximum weight.")
+// } else {
+//     console.log("No extra fees, have a nice flight!")
+// }
+
+
+
+
+
+
+// replace the blanks with a conditional statement that prints the correct statement depending on the carSpeed that you input.
+let carSpeed //  = _____
+const maximumSpeed = 70
+const minimumSpeed = 45
+
+
+// if (__________) {
+//     console.log("Slow down, you're speeding!")
+// } else if (__________) {
+//     console.log("Speed up, you're holding up traffic!")
+// } else {
+//     console.log("You are at a great speed, just keep driving.")
+// }
+
+
+
+
+// challenge: write a conditional statement that mimics an automatic checkout machine at a store.
+// if customerPayment is greater than the totalCheckoutCost, calculate the customer's change and print a statement like this:
+        // Your change is $3.25. Thank you for shopping with us today.
+// if customerPayment is less than the totalCheckoutCost, calculate the remaining balance and print a statement like this:
+        // There is still $2.75 remaining on your balance.
+// if customerPayment is equal to the totalCheckoutCost, print a statement wishing the customer a nice day.
+        // Thanks and have a nice day!
+
+let customerPayment //  = _______
+const totalCheckoutCost //  = _______
+// write conditional statement here:
