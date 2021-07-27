27aa3d174995e948c307ffe7584a6c8f8d9ffc5e
Jeff <jolson615@gmail.com>
2021-07-26 20:04:22 -0400

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,8 @@
-/*
+/* ==== WARM-UP SET 1 ====
 Replace the '__' with comparison operators to make the following statements true
 Try to use a different comparison operator each time
 */ 
+console.log("The following lines should say TRUE")
 // console.log(45 __ 45)
 // console.log(-9 __ 9)
 // console.log(100 __ 100000)
@@ -9,19 +10,21 @@ Try to use a different comparison operator each time
 // console.log("hello" __ "dog")
 // console.log("Danny" __ "Danny")
 
-/* 
+
+/* ==== WARM-UP SET 2 ====
 Replace the '__' with comparison operators to make the following statements false
 Try to use a different comparison operator each time
 */
+console.log("\nThe following lines should say FALSE")
 // console.log(2 __ 10)
 // console.log(22 __ 22)
 // console.log( 0 __ -5)
 // console.log(3.3 __ 3.5)
 // console.log("frog" __ "prince")
 
-// -----------------------------------------------------------------------------------
 
-/* 
+console.log("\nExercises start here.")
+/* ==== EXERCISE 1 ====
 replace the blank with a conditional statement so that the lunch invitation is only printed if isHungry is true. Change the boolean in the isHungry variable to check and see if your conditional statement works.
 */
 let isHungry = false
@@ -30,7 +33,7 @@ let isHungry = false
 //     console.log("Do you want to grab some lunch?")
 // }
 
-/* 
+/* ==== EXERCISE 2 ====
 Replace the blank with a conditional statement that tests to see if a number is positive or negative.
 */
 // let firstNumber = ______
@@ -40,7 +43,7 @@ Replace the blank with a conditional statement that tests to see if a number is 
 //     console.log("That number is positive!")
 // }
 
-/*
+/* ==== EXERCISE 3 ====
 Replace the blank with a conditional statement that tests to see if a number is even. Hint: the % operator might be useful here.
 */
 // let secondNumber = ______
@@ -53,7 +56,7 @@ Replace the blank with a conditional statement that tests to see if a number is 
 
 
 
-/* 
+/* ==== EXERCISE 4 ====
 Replace the blank with a conditional statement that only adds a fee to the ticket price if the luggage weight is over 50 pounds
 */
 
@@ -71,7 +74,7 @@ Replace the blank with a conditional statement that only adds a fee to the ticke
 
 
 
-/* 
+/* ==== EXERCISE 5 ====
 replace the blanks with a conditional statement that prints the correct statement depending on the carSpeed that you input.
 */ 
 // let carSpeed = _____
@@ -89,16 +92,21 @@ const minimumSpeed = 45
 
 
 
-/* 
-challenge: write a conditional statement that mimics an automatic checkout machine at a store.
+/* ==== EXERCISE 6: CHALLENGE ====
+Write a conditional statement that mimics an automatic checkout machine at a store.
+
+===== This example is pseudocode, but it describes what our challenge program should do! ======
+if customerPayment is greater than the totalCheckoutCost, calculate the customer's change and print a statement like this:
+        Your change is $3.25. Thank you for shopping with us today.
+if customerPayment is less than the totalCheckoutCost, calculate the remaining balance and print a statement like this:
+        There is still $2.75 remaining on your balance.
+if customerPayment is equal to the totalCheckoutCost, print a statement wishing the customer a nice day.
+        Thanks and have a nice day!
 */
-// if customerPayment is greater than the totalCheckoutCost, calculate the customer's change and print a statement like this:
-        // Your change is $3.25. Thank you for shopping with us today.
-// if customerPayment is less than the totalCheckoutCost, calculate the remaining balance and print a statement like this:
-        // There is still $2.75 remaining on your balance.
-// if customerPayment is equal to the totalCheckoutCost, print a statement wishing the customer a nice day.
-        // Thanks and have a nice day!
 
 // let customerPayment = _______
 // let totalCheckoutCost = _______
-// write conditional statement here:
+
+/* 
+write conditional statement below:
+*/
